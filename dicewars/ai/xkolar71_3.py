9b84c42991da59a060ada7582b5ebc3dbab82834
Josef Kolář <thejoeejoee@gmail.com>
2020-10-20 23:56:34 +0200

    Added siblings to working AI xkolar71

---
+++ dicewars/ai/xkolar71_3.py
@@ -0,0 +1,3 @@
+from .xkolar71 import AI
+
+__all__ = ['AI']
---
+++ scripts/dump-data-to-learn.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+import json
+import pickle
+import random
+import sys
+from argparse import ArgumentParser
+from signal import signal, SIGCHLD
+
+from utils import run_ai_only_game, get_nickname, BoardDefinition, SingleLineReporter, PlayerPerformance, \
+    TournamentCombatantsProvider, EvaluationCombatantsProvider, column_t
+
+parser = ArgumentParser(prog='Dice_Wars')
+parser.add_argument('-p', '--port', help="Server port", type=int, default=5005)
+parser.add_argument('-a', '--address', help="Server address", default='127.0.0.1')
+parser.add_argument('-b', '--board', help="Seed for generating board", type=int, default=random.randint(0, 10 ** 10))
+parser.add_argument('-s', '--seed', help="Seed sampling players for a game", type=int)
+
+procs = []
+
+
+def signal_handler(signum, frame):
+    """Handler for SIGCHLD signal that terminates server and clients
+    """
+    for p in procs:
+        try:
+            p.kill()
+        except ProcessLookupError:
+            pass
+
+
+PLAYING_AIs = [
+    'xkolar71',
+    'xkolar71_1',
+    'xkolar71_2',
+    'xkolar71_3',
+]
+UNIVERSAL_SEED = 42
+
+
+def board_definitions(initial_board_seed):
+    board_seed = initial_board_seed
+    while True:
+        yield BoardDefinition(board_seed, UNIVERSAL_SEED, UNIVERSAL_SEED)
+        if board_seed is not None:
+            board_seed += 1
+
+
+def main():
+    args = parser.parse_args()
+
+    random.seed(args.seed)
+
+    signal(SIGCHLD, signal_handler)
+
+    boards_played = 0
+    try:
+        for board_definition in board_definitions(args.board):
+            boards_played += 1
+
+            run_ai_only_game(
+                args.port, args.address, procs, PLAYING_AIs,
+                board_definition,
+                fixed=UNIVERSAL_SEED,
+                client_seed=UNIVERSAL_SEED,
+                debug=True, logdir='logs',
+            )
+
+            if boards_played == 20:
+                break
+
+    except (Exception, KeyboardInterrupt) as e:
+        sys.stderr.write("Breaking the tournament because of {}\n".format(repr(e)))
+        for p in procs:
+            p.kill()
+        raise
+
+
+if __name__ == '__main__':
+    main()
