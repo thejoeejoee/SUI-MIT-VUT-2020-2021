00a2224b31e6fa5c8ea3335de5d17927391ab96f
Josef Kolář <thejoeejoee@gmail.com>
2020-10-21 21:45:24 +0200

    Improved script for loading configurations into numpy array

--- ml-scripts/transform-to-numpy.py
+++ ml-scripts/transform-to-numpy.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from os import listdir
 
 import numpy as np
@@ -7,23 +8,34 @@ DATA_DIR = os.path.join(os.path.dirname(__file__), '../learning-data')
 
 winners = listdir(DATA_DIR)
 
-data = np.empty((0, 556), dtype=int)
+PACKAGES = dict(
+    train=slice(0, 17),
+    val=slice(17, 17 + 5),
+    test=slice(17 + 5, 17 + 5 + 3),
+)
 
-confs_count = 0
+for name, slc in PACKAGES.items():
+    slc = slice(slc.start * 1000, slc.stop * 1000)
 
-for winner in winners:
-    winner_id = int(winner)
+    loaded_files = 0
+    data = np.empty((0, 556), dtype=int)
 
-    for conf in listdir(os.path.join(DATA_DIR, winner))[8000:8000+8000]:
-        pth = os.path.join(DATA_DIR, winner, conf)
+    for winner in winners:
+        if winner not in '1234':
+            continue
 
-        line = np.concatenate(([winner_id - 1], np.load(pth, allow_pickle=True)))
-        data = np.concatenate(
-            ([line], data),
-        )
-        confs_count += 1
+        winner_id = int(winner)
 
-        if not confs_count % 1000:
-            print(f'Loaded {confs_count} confs.')
+        for conf in listdir(os.path.join(DATA_DIR, winner))[slc]:
+            pth = os.path.join(DATA_DIR, winner, conf)
 
-np.save(os.path.join(DATA_DIR, 'learning-data-32k-02'), data)
+            line = np.concatenate(([winner_id - 1], np.load(pth, allow_pickle=True)))
+            data = np.concatenate(
+                ([line], data),
+            )
+            loaded_files += 1
+
+            if not loaded_files % 1000:
+                print(f'Loaded {loaded_files} configurations for {name}.', file=sys.stderr)
+
+    np.save(os.path.join(DATA_DIR, f'learning-data-{name}'), data)
